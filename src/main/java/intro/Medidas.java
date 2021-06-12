58d2c84170732dbf24b9ff761f41f239e5117cfa
ASUS <tamarasilvagomes@mail.com>
2021-06-12 11:41:52 -0300

    Exclui o "Bom dia!!!" no começo da impressão de texto do Console
    Criei o método calcularAreaModoCurto e movi o código que estava em main para lá
    
    O método main agora chama os 2 métodos calcular* que passaram também a ser static

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -9,9 +9,14 @@ public class Medidas {
 
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
-        System.out.println("Bom dia!!!");
-        System.out.println("Cálculo de Areas");
 
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
+
+    }
+
+    public static void calcularAreaModoCurto() {
+        System.out.println("Cálculo de Areas modo Curto");
 
         // Calculo de área - Reduzido
         int largura = 4;
@@ -19,12 +24,12 @@ public class Medidas {
 
         System.out.println("Para a largura de " + largura + "m e o comprimento de "
                 + comprimento + "m a área é de " + largura * comprimento + "m²");
-
-
-
     }
 
-        public void calcularAreaModoExtenso(){
+    public static void calcularAreaModoExtenso(){
+
+        System.out.println("Cálculo de Areas Modo Extenso");
+
         // Calculo de área - Exemplo: o tamanho do tapete ou piso
         int largura;
         int comprimento;
